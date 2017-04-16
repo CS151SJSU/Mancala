1be1eee53babeedbb7257e6fec746c2aa9a6c789
rahuldalal <rahul.v.dalal@gmail.com>
2017-04-16 11:45:20 -0700

    initial commit with Data Model v1.0

---
+++ src/Hole.java
@@ -0,0 +1,52 @@
+
+public class Hole implements Cloneable {
+
+	public static final int MANCALA_HOLE = 1;
+	public static final int NORMAL_HOLE = 0;
+	private int id; // i do the hole from 0 to 13
+	private int stonesCount; // No. of stones currently in the hole
+	private boolean isEmpty; // Is the hole empty
+	private int belongsto; // id of the player that the hole belongs to (0 or 1)
+	private int type; // Normal or Mancala
+	
+	public Hole (int type)
+	{
+		if(type != MANCALA_HOLE && type != NORMAL_HOLE) throw new IllegalArgumentException();
+		this.type = type;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getStonesCount() {
+		return stonesCount;
+	}
+	public void setStonesCount(int stonesCount) {
+		this.stonesCount = stonesCount;
+	}
+	public boolean isEmpty() {
+		return isEmpty;
+	}
+	public void setEmpty(boolean isEmpty) {
+		this.isEmpty = isEmpty;
+	}
+	public int getBelongsto() {
+		return belongsto;
+	}
+	public void setBelongsto(int belongsto) {
+		this.belongsto = belongsto;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public Object clone()throws CloneNotSupportedException{  
+		return super.clone();  
+		} 
+	
+}
---
+++ src/Model.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+import javax.swing.event.ChangeListener;
+
+public class Model {
+	
+	private ArrayList<Hole> data;
+	private int currentHole; //at what hole should you start the current turn
+	private boolean isFirstPlayerTurn; // Indicates whehter it is the turn of player 1 or player 2
+	private ArrayList<ChangeListener> listeners;
+	
+	// Get the entire list of Holes
+	public ArrayList<Hole> getData() {
+		return (ArrayList<Hole>) data.clone();
+	}
+	
+	// Getter and setter for the current hole
+	public int getCurrentHole() {
+		return currentHole;
+	}
+	public void setCurrentHole(int currentHole) {
+		this.currentHole = currentHole;
+	}
+	
+	// Getter and setter for for boolean variable indicating the whether it's the firs player's turn
+	public boolean getIsFirstPlayerTurn() {
+		return isFirstPlayerTurn;
+	}
+
+	public void setIsFirstPlayerTurn(boolean isFirstPlayerTurn) {
+		this.isFirstPlayerTurn = isFirstPlayerTurn;
+	}
+
+	// Returns null in case of any exception; in case of correct index returns the object
+	public Hole getHole(int id)
+	{
+		try {
+			return (Hole) data.get(id).clone();
+		} catch (CloneNotSupportedException e1) {
+			e1.printStackTrace();
+		}
+		catch (IndexOutOfBoundsException e2) {
+			e2.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void attach(ChangeListener c) {
+		listeners.add(c);
+	}
+	
+	// Mutator of data model that gets called after the player plays a turn i.e Controller updates data
+	public void updateBoard() {
+		
+	}
+	
+	
+
+}
